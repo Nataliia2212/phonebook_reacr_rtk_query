96940513c9daaedef258ba9f8e8bd34c575ec461
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-03-10 00:44:58 +0200

    navigate

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,15 +3,17 @@ import React from 'react';
 import { Form } from '../components/Form/Form';
 
 import { useSignupUserMutation } from '../redux/contactsApi';
+import { useNavigate } from 'react-router-dom';
 
 export const Register = () => {
   const [addNewUser] = useSignupUserMutation();
+  const navigate = useNavigate();
 
   const handleSubmit = data => {
     addNewUser(data)
       .unwrap()
       .then(res => {
-        localStorage.setItem('token', res.token);
+        navigate('/contacts');
       });
   };
 
