8e21cf4b6f86ec2be1954c76978f49150f29b4cb
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-03-11 11:07:27 +0200

    path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
+import { Phonebook, Login, Contacts, Register } from './pages/index';
+import { Preloader, PrivateRoute, PublicRoute } from './components';
 import Layout from './components/Layout/Layout';
-import { Phonebook } from './pages/Phonebook/Phonebook';
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { Contacts } from './pages/Contacts';
-import { PrivateRoute } from './components/routes/PrivateRoute';
-import { PublicRoute } from './components/routes/PublicRoute';
-import { useSelector } from 'react-redux';
+
 import { selectIsRefresh, selectToken } from './redux/userSlice';
 import { useCurrentUserQuery } from './redux/contactsApi';
-import { Preloader } from './components/Preloader/Preloader';
 
 export default function App() {
   const token = useSelector(selectToken);
